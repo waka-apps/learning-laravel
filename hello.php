45dc63509c30637d478a4ad0960b05f13796bb60
waka-apps <waka.apps@gmail.com>
2026-06-25 23:48:53 +0900

    Include learning files with journal commits

---
+++ hello.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = 'Wakabayashi';
+$years = 10;
+
+echo "Hello, {$name}\n";
+echo "Experience: {$years} years\n";
+
+var_dump($name);
+var_dump($years);
+var_dump($years + '5');
+var_dump(gettype($years));

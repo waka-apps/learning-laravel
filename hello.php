fa4c839649710aebc4b91996b193f7e206e3b897
waka-apps <waka.apps@gmail.com>
2026-06-28 16:16:44 +0900

    Add Phase 2 lesson 3 journal

--- hello.php
+++ hello.php
@@ -1,29 +1,47 @@
 <?php
 declare(strict_types=1);
 
+enum ShipmentStatus: string
+{
+    case Accepted = "accepted";
+    case InTransit = "in_transit";
+    case Delivered = "delivered";
+}
+
 class Shipment
 {
     public function __construct(
         public readonly string $trackingNumber,
-        private string $status,
+        private ShipmentStatus $status,
     ) {}
 
     public function status(): string
     {
-        return $this->status;
+        return $this->status->value;
     }
 
     public function markDelivered(): void
     {
-        $this->status = "delivered";
+        $this->status = ShipmentStatus::Delivered;
+    }
+
+    public function statusLabel(): string
+    {
+        return match ($this->status) {
+            ShipmentStatus::Accepted => "Accepted",
+            ShipmentStatus::InTransit => "In transit",
+            ShipmentStatus::Delivered => "Delivered",
+        };
     }
 }
 
-$shipment = new Shipment("SHP-001", "in_transit");
+$shipment = new Shipment("SHP-001", ShipmentStatus::InTransit);
 
 var_dump($shipment->trackingNumber);
 var_dump($shipment->status());
+var_dump($shipment->statusLabel());
 
 $shipment->markDelivered();
 
 var_dump($shipment->status());
+var_dump($shipment->statusLabel());

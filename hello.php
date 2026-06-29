21d0005dd87615a7d7e83a9b0826aeae6e52a0e5
waka-apps <waka.apps@gmail.com>
2026-06-29 12:07:22 +0900

    Add Phase 2 lesson 4 journal

--- hello.php
+++ hello.php
@@ -35,13 +35,37 @@ class Shipment
     }
 }
 
-$shipment = new Shipment("SHP-001", ShipmentStatus::InTransit);
+function findShipment(array $shipments, string $trackingNumber): ?Shipment
+{
+    foreach ($shipments as $shipment) {
+        if ($shipment->trackingNumber === $trackingNumber) {
+            return $shipment;
+        }
+    }
 
-var_dump($shipment->trackingNumber);
-var_dump($shipment->status());
-var_dump($shipment->statusLabel());
+    return null;
+}
 
-$shipment->markDelivered();
+function requireShipment(array $shipments, string $trackingNumber): Shipment
+{
+    $shipment = findShipment($shipments, $trackingNumber);
 
-var_dump($shipment->status());
-var_dump($shipment->statusLabel());
+    if ($shipment === null) {
+        throw new Exception("Shipment not found: {$trackingNumber}");
+    }
+
+    return $shipment;
+}
+
+$shipment1 = new Shipment("SHP-001", ShipmentStatus::InTransit);
+$shipment2 = new Shipment("SHP-002", ShipmentStatus::InTransit);
+$shipment3 = new Shipment("SHP-003", ShipmentStatus::InTransit);
+$shipments = [$shipment1, $shipment2, $shipment3];
+
+var_dump(findShipment(shipments: $shipments, trackingNumber: "SHP-999"));
+
+try {
+    var_dump(requireShipment(shipments: $shipments, trackingNumber: "SHP-999"));
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";
+}
